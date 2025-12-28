da3e2fa805ec34e8763265324c35f0a448aecd0e
Vincent Marchetti <vmarchetti@kshell.com>
2025-12-28 12:45:12 -0500

    Bringing in TranslateTransform

--- packages/transforms/test/index.js
+++ packages/transforms/test/index.js
@@ -1,20 +1,15 @@
-import {makeRotateTransform} from "./testSupport.js";
+import {T} from "./testSupport.js";
 import {Transform} from "../dist/index.js"
 import {expect} from "chai";
 
 
 describe("first test", function(){
-    let R;
-    let T;
     
-    it("createRotationTransform", () => {        
-        R = makeRotateTransform(90.0,0,0);
-        expect(R).to.exist;
+    it("creates lists", () => {        
+        let transList = [T(1.0,2.0,3.0)];
+        expect(transList).to.exist;
     });
     
-    it("create Transfrom from R", () =>{
-        T = Transform.from_manifesto_transform(R);
-        expect(T).to.exist;
-    })
+
 });
 
--- packages/transforms/test/testSupport.js
+++ packages/transforms/test/testSupport.js
@@ -1,16 +1,6 @@
-import {RotateTransform} from "manifesto-prezi4/index.js";
+import * as manifesto from "manifesto-prezi4/index.js";
 
 
-export function makeRotateTransform(xv,yv,zv){
-    return new RotateTransform(
-        {
-            "type":"RotateTransform",
-            "x" : xv,
-            "y" : yv,
-            "z" :zv
-        }
-    );
-};
 
 export function T(xv,yv,zv){
     const jsonld = {
