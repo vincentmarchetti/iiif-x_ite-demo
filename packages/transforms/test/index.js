88bc3eca6280ea410b4fd1a5d25fd7f5ca26d410
Vincent Marchetti <vmarchetti@kshell.com>
2025-12-28 12:59:49 -0500

    Extened unit testing to include transformsToTest, this runs correctly and succeed in Mocha at this commit

--- packages/transforms/test/index.js
+++ packages/transforms/test/index.js
@@ -1,15 +1,24 @@
 import {T} from "./testSupport.js";
-import {Transform} from "../dist/index.js"
+import {Transform, transformsToPlacements} from "../dist/index.js"
 import {expect} from "chai";
 
+function labels(aList){
+    return aList.reduce( (accum,t) => {return accum+t.label;},"[") + "]";
+}
 
 describe("first test", function(){
     
     it("creates lists", () => {        
         let transList = [T(1.0,2.0,3.0)];
         expect(transList).to.exist;
+        
+        expect(labels(transList)).to.be.a('string');
+        
+        let tlist = transList.map(Transform.from_manifesto_transform);
+        expect(tlist).to.be.a('array');
+        
+        let placements = transformsToPlacements(tlist);
+        expect(placements).to.be.a('array');
     });
-    
-
 });
 
