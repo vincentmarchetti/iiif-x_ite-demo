fd607673e711b1ddb51620f9fc17ebfc58efa977
Vincent Marchetti <vmarchetti@kshell.com>
2025-12-28 17:09:48 -0500

    Added to be the argument of a --requre option for mocha, so these operations will only be executed once (registering custom plugins)

---
+++ packages/transforms/test/setup.js
@@ -0,0 +1,12 @@
+
+
+
+const chai = require("chai");
+const chaiAlmost = require("chai-almost");
+const chaiTransforms = require("../packages/chai-transforms/index.js");
+chai.use(chaiAlmost());
+chai.use(chaiTransforms());
+//console.log(`require-in chaiAlmost : ${chaiAlmost}`);
+
+
+
