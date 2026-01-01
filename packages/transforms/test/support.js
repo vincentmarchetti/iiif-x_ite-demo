145f367cd6ad3d388e5bcb6aeadb78015ba94b14
Vincent Marchetti <vmarchetti@kshell.com>
2026-01-01 13:09:10 -0500

    Added toString support for Vector3 and for S,R,T return values

--- packages/transforms/test/support.js
+++ packages/transforms/test/support.js
@@ -1,4 +1,9 @@
 const manifesto = require( "manifesto-prezi4/index.js");
+const Vector3 = require("threejs-math").Vector3;
+
+Vector3.prototype.toString =  function(){
+    return `Vector3(${this.x}, ${this.y}, ${this.z})`;
+};
 
 
 
@@ -10,7 +15,13 @@ function T(xv,yv,zv){
         "z"    : zv
     };
     const u =  new manifesto.TranslateTransform(jsonld);
-    u.label = "T"
+    u.label = "T";
+    
+    
+    u.toString = function(){
+        return `T(${xv} , ${yv}, ${zv})`;
+    };
+   
     return u;
 };
 
@@ -22,7 +33,12 @@ function R(xv,yv,zv){
         "z"    : zv
     };
     const u =  new manifesto.RotateTransform(jsonld);
-    u.label = "R"
+    u.label = "R";
+    
+    u.toString = function(){
+        return `R(${xv} , ${yv}, ${zv})`;
+    };
+
     return u;
 };
 
@@ -34,8 +50,11 @@ function S(xv,yv,zv){
         "z"    : zv
     };
     const u =  new manifesto.ScaleTransform(jsonld);
-    u.label = "S"
+    u.label = "S";
+    u.toString = function(){
+        return `S(${xv} , ${yv}, ${zv})`;
+    };
     return u;
 };
 
-module.exports = {S,R,T};
+module.exports = {S,R,T, Vector3};
