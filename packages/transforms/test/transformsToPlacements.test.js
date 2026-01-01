4c893f5b6656f2e268641428133e8038442dc2ac
Vincent Marchetti <vmarchetti@kshell.com>
2026-01-01 16:40:30 -0500

    continued implementation, at this commit the testing is functioning in that it is concisely reporting problems with placementsToTransforms functions

--- packages/transforms/test/transformsToPlacements.test.js
+++ packages/transforms/test/transformsToPlacements.test.js
@@ -4,7 +4,12 @@ import {expect} from "chai";
 import {S,R,T,Vector3} from "./support.js";
 import {Transform, transformsToPlacements} from "../dist-commonjs/index.js"
 
-
+function applyListToVector3(operatorList, vect){
+    return operatorList.reduce( function(accum,operator){
+        return operator.applyToVector3(accum);
+    },
+    vect);
+}
 const testCases = [
         [S(1,1,1),R(90,0,0),T(1.0,2.0,3.0)],
         [T(1.0,2.0,3.0),R(90,0,0),S(1,1,1)],
@@ -24,6 +29,16 @@ describe(`transformsToPlacements`, function(){
             
             let placements = transformsToPlacements(transformsList);
             expect(placements).to.be.a('array');
+            
+            const testCoords = [0.0,1.0];
+            for (let i = 0; i<2;++i)
+            for (let j = 0; j<2;++j)
+            for (let k = 0; k<2;++k){
+                const target = new Vector3(testCoords[i],testCoords[j],testCoords[k]);
+                const applied_transforms = applyListToVector3(transformsList, target);
+                const applied_placements = applyListToVector3(placements    , target);
+                expect(applied_placements).to.almost.equal(applied_transforms);
+            }
         });
     });
 });
