ec6af7152059967302a957ed913b7edf741e166a
Vincent Marchetti <vmarchetti@kshell.com>
2026-01-01 13:06:13 -0500

    cleaned up whitespace and commented out code

--- packages/transforms/test/setup.js
+++ packages/transforms/test/setup.js
@@ -1,12 +1,7 @@
-
-
-
 const chai = require("chai");
 const chaiAlmost = require("chai-almost");
 const chaiTransforms = require("../packages/chai-transforms/index.js");
 chai.use(chaiAlmost());
 chai.use(chaiTransforms());
-//console.log(`require-in chaiAlmost : ${chaiAlmost}`);
-
 
 
