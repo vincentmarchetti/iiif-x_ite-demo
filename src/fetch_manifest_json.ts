d291e339a4458338b251e572211b9c887763fbf5
Vincent Marchetti <vmarchetti@kshell.com>
2026-02-06 17:48:49 -0500

    Moved the implementation of a fallback manifest url to this function

--- src/fetch_manifest_json.ts
+++ src/fetch_manifest_json.ts
@@ -1,7 +1,8 @@
 
 
 let MANIFEST_KEY : string = "manifest";
-export async function fetch_manifest_json(viewer_url_string : string ) : object | null {
+export async function fetch_manifest_json(viewer_url_string : string ,
+                    fallback_manifest_url : string|null = null) : object | null {
     /**
     *  extracts a URI, either a URL network location
     *  or a data URI, and does what is necessary to
@@ -10,6 +11,9 @@ export async function fetch_manifest_json(viewer_url_string : string ) : object 
     *  If a value is not found under MANIFEST_KEY in the query part
     *  of window.location then the function will use a variable
     *  fallback_manifest_url , defined in global scope
+    *
+    *  The fallback_manifest_url mechanism is primarly intended
+    *  for debug and development
     *
     *  otherwise a value of null is returned
     *
@@ -21,20 +25,23 @@ export async function fetch_manifest_json(viewer_url_string : string ) : object 
     
     // reference : https://developer.mozilla.org/en-US/docs/Web/API/URL
     const viewer_url : URL = new URL(viewer_url_string);
-    const manifest_url_string : string | null = 
+    const manifest_url : string | null = 
         viewer_url.searchParams.get( MANIFEST_KEY ) || fallback_manifest_url;
     
-    if ( manifest_url_string  == null ) return null;
-    
+    if ( manifest_url  == null ) return null;
+    console.log(`fetch_manifest_json : fetching ${manifest_url}`);
     // reference : https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
-    const response = await fetch( manifest_url_string );
+    const response = await fetch( manifest_url );
+    
     if (!response.ok) {
-      throw new Error(`Fetch of manifest failed: Response status: ${response.status}`);
+        throw new Error(`Fetch of manifest failed: Response status: ${response.status}`);
     }
-    try {
+    try {        
         const result = await response.json();
+        console.log(`fetch_manifest_json parsed result: type ${result.type}`);
         return result;
     } catch (error : unknown ){
+        console.error(`fetch_manifest_json :${error}`);
         const message = ( (x:unknown):string => {
             if (x instanceof Error) return x.message;
             return String(x);
